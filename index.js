e10b26f8a6627a08b016776a9b7c8aa7a6f87bda
Mishal Aslam <101046955+mishal-aslam@users.noreply.github.com>
2023-02-17 23:18:01 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,68 @@
+#! /usr/bin/env node
+import inquirer from 'inquirer';
+class Todo {
+    items = [];
+    constructor() { }
+    add(item) {
+        this.items.push(item);
+    }
+    remove(index) {
+        this.items.splice(index, 1);
+    }
+    list() {
+        console.log(`You have ${this.items.length} items in your Todo list:`);
+        this.items.forEach((item, index) => {
+            console.log(`${index + 1}: ${item}`);
+        });
+    }
+}
+const todo = new Todo();
+function main() {
+    inquirer
+        .prompt([
+        {
+            type: 'list',
+            name: 'operation',
+            message: 'What do you want to do?',
+            choices: ['Add', 'Remove', 'List', 'Quit'],
+        },
+    ])
+        .then((answers) => {
+        if (answers.operation === 'Add') {
+            inquirer
+                .prompt([
+                {
+                    type: 'input',
+                    name: 'item',
+                    message: 'Enter the Todo item:',
+                },
+            ])
+                .then((answers) => {
+                todo.add(answers.item);
+                main();
+            });
+        }
+        else if (answers.operation === "Remove") {
+            inquirer.prompt([
+                {
+                    type: "input",
+                    name: "index",
+                    message: "Enter the index of todo item"
+                },
+            ])
+                .then((answers) => {
+                const index = parseInt(answers.index, 10);
+                todo.remove(index - 1);
+                main();
+            });
+        }
+        else if (answers.operation === "List") {
+            todo.list();
+            main();
+        }
+        else {
+            console.log("Todo-List-END");
+        }
+    });
+}
+main();
